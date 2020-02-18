3f2375f8c4e0f6f4be9fa3fa1a68ba74d4ac3282
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2020-02-17 21:21:39 -0600

    wip

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
+import Publications from '../views/Publications.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/contacto',
     name: 'Contact',
     component: Contact
+  },
+  {
+    path: '/publicaciones',
+    name: 'Publications',
+    component: Publications
   },
   {
     path: '/about',
---
+++ src/views/Publications.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="publications">
+    <SiteNavbar></SiteNavbar>
+    <PublicationsTitle></PublicationsTitle>
+    <SiteFooter></SiteFooter>
+  </div>
+</template>
+
+<script>
+import SiteNavbar from '@/components/SiteNavbar'
+import PublicationsTitle from '@/components/PublicationsTitle'
+import SiteFooter from "@/components/SiteFooter"
+export default {
+    name: 'Publications',
+    components: {
+        SiteNavbar,
+        PublicationsTitle,
+        SiteFooter
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
